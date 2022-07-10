b9fa56b430f1951ead1774ec2d78bf873e71f6e3
Aliev Qara <alievqara@outlook.com>
2022-07-10 22:36:01 +0400

    tasktwo

---
+++ homework2/Invoice.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework2
+{
+    public class Invoice
+    {
+
+
+        public string Article { get; set; }
+        public int Quantity { get; set; }
+
+        int account;
+        string customer;
+        string provider;
+
+        bool needEdv = false;
+
+
+
+        public Invoice()
+        {
+
+        }
+
+        public void CostColculation(bool needEdv)
+        {
+            this.needEdv = needEdv;
+            decimal sum = 0;
+            decimal edv = account * 18 / 100;
+
+            if (needEdv == true)
+            {
+                sum = account * Quantity + edv;
+                Console.WriteLine(sum);
+            }
+            else
+            {
+                sum = account * Quantity;
+                Console.WriteLine(sum);
+            }
+        }
+
+        public Invoice(int account, string customer, string provider) : this()
+        {
+            this.account = account;
+            this.customer = customer;
+            this.provider = provider;
+
+
+        }
+
+
+
+
+    }
+}
---
+++ homework2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+
+namespace homework2
+{
+    public class Program
+    {
+
+
+        public static void Main()
+        {
+
+            Invoice mehsul1 = new Invoice(13, "Faig", "Aliyev")
+            {
+                Article = "USB Hub",
+                Quantity = 6
+
+            };mehsul1.CostColculation(false);
+            
+
+            Invoice mehsul2 = new Invoice(13, "Faig", "Aliyev")
+            {
+                Article = "Laptop",
+                Quantity = 1400
+
+            };mehsul2.CostColculation(false);
+
+
+            Invoice mehsul3 = new Invoice(13, "Faig", "Aliyev")
+            {
+                Article = "SD-Card",
+                Quantity = 30
+
+            }; mehsul3.CostColculation(true);
+
+            Invoice mehsul4 = new Invoice(13, "Faig", "Aliyev")
+            {
+                Article = "USB",
+                Quantity = 12
+
+            }; mehsul4.CostColculation(false);
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+
+}
